28167f47172652b71d75a6142e1170322050faa8
brurez <brunoderezende@hotmail.com>
2018-12-23 19:40:08 -0200

    Increasing accuracy using standard deviation

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -3,9 +3,17 @@ require("@tensorflow/tfjs-node");
 const loadCSV = require("./load-csv");
 
 function knn(features, labels, predition, k) {
+  const { mean, variance } = tf.moments(features, 0);
+
+  const scaledPrediction = predition
+    .sub(mean)
+    .div(variance.pow(0.5));
+
   return (
     features
-      .sub(predition)
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
       .pow(2)
       .sum(1)
       .pow(0.5)
@@ -23,7 +31,7 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   {
     shuffle: true,
     splitTest: 10,
-    dataColumns: ["lat", "long", "sqft_lot"],
+    dataColumns: ["lat", "long", "sqft_lot", 'sqft_living'],
     labelColumns: ["price"]
   }
 );
@@ -31,11 +39,10 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
 features = tf.tensor(features);
 labels = tf.tensor(labels);
 
-
-testFeatures.forEach( (test, i) => {
+testFeatures.forEach((test, i) => {
   const result = knn(features, labels, tf.tensor(test), 10);
   const err = (testLabels[i][0] - result) / testLabels[i][0];
 
   //console.log('Guess', result, testLabels[i][0]);
-  console.log('Error', err * 100);
+  console.log("Error", err * 100);
 });
